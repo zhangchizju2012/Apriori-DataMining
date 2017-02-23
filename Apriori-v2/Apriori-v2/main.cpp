91011cef2a1bdce0d80021edf9eceab86beda18a
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-02-22 23:28:26 -0700

    add comments

--- Apriori-v2/Apriori-v2/main.cpp
+++ Apriori-v2/Apriori-v2/main.cpp
@@ -82,6 +82,8 @@ public:
     std::map<std::vector<int>,std::vector<int>> candidateGen(std::map<std::vector<int>,std::vector<int>> myvector,
                                                              int threshold,
                                                              std::vector<std::vector<int>> tempResult){
+        // data structure of myvector
+        // key: itemset with min_support; value: transactions which have itemset
         std::map<std::vector<int>,std::vector<int>> result;
         std::vector<int> convergeResult;
         std::vector<int> key;
@@ -172,7 +174,7 @@ public:
     
     std::map<std::vector<int>,std::vector<int>> firstPass(std::unordered_map<int,std::vector<int>> freqList,
                                                           int threshold){
-        //for the first time
+        //C2 = candidates generated from L1
         std::vector<int> tempVector;
         for ( auto it = freqList.begin(); it != freqList.end(); ++it ){
             if(it->second.size()>=threshold){
@@ -196,6 +198,10 @@ public:
 };
 
 vector< vector<int> > getAllSubsets(vector<int> set)
+// Generate all subsets for a itemset (except empty and itself),
+// this function is used in generating association rules
+// eg. itemset = {2,3,5}, this function will return
+// {2},{3},{5},{2,3},{2,5},{3,5} ({} and {2,3,5} is not included)
 {
     vector< vector<int> > subset;
     vector<int> empty;
@@ -215,6 +221,11 @@ vector< vector<int> > getAllSubsets(vector<int> set)
 }
 
 vector<int> getRemain(vector<int> all, vector<int> subset){
+// Generate a vector which is in vector 'all' but not in vector 'subset'
+// both 'all' and 'subset' are sorted
+// this function is used in generating association rules
+// eg. all = {2,3,5}, subset = {2,3}
+// this function will return {5}
     vector<int> remain;
     int i = 0;
     int j = 0;
@@ -238,45 +249,61 @@ vector<int> getRemain(vector<int> all, vector<int> subset){
 int getAssociationRuleNumber(std::unordered_map<int,std::map<std::vector<int>,int>> freqItemSetsAndNumber,
                              std::vector<std::vector<std::vector<int>>> result,
                              float confidence, int lineNumber, bool show=false){
+    // Data structure of freqItemSetsAndNumber:
+    // key: size of each itemset (eg. freqItemSet:(2,3,4), size is 3), value: a map, called MAP, see below
+    // MAP's key: itemset with min_support, all itemsets here have same size;
+    // MAP's value: number of transactions which have itemset
+    // eg. itemset size -> {2:({item6,item7}:{trans1},{item4,item8}:{trans3,trans8,trans11}),
+    //     itemset size -> 3:({item1,item2,item3}:{trans1,trans2,trans5,trans8},{item1,item4,item8}:{trans2,trans8})}
+    
+    // each member in result is itemset with min_support
+    
+    // Generating Association Rules from Frequent Itemsets
+    // Algorithm Description:
+    // For each frequent itemset, f, generate all non-empty subsets of f.
+    // For every non-empty subset s of f do
+    //      output rule s->(f-s) if support(f)/support(s) >= min_confidence
+    // end
     vector< vector<int> > subsets;
-    int denominator;
+    int denominator; //#X
     unsigned long denominatorLength;
-    int numerator;
+    int numerator; //#(X&Y)
     unsigned long numeratorLength;
-    int associationRuleNumber = 0;
+    int associationRuleNumber = 0;//count association Rule number
     vector<int> remain;
-    ofstream file_("/Users/zhangchi/Desktop/Apriori-DataMining/Apriori-v2/Apriori-v2/rule.txt");
+    ofstream file_("/Users/zhangchi/Desktop/Apriori-DataMining/Apriori-v2/Apriori-v2/rule.txt"); //for rule.txt file
     for(int i=1; i<result.size()-1; i++){
         for(int j=0;j<result.at(i).size();j++){
             numeratorLength = result.at(i).at(j).size();
             auto it = freqItemSetsAndNumber.find(int(numeratorLength));
             auto ij = it->second.find(result.at(i).at(j));
-            numerator = ij->second;
+            numerator = ij->second; //#(X&Y)
             subsets = getAllSubsets(result.at(i).at(j));
             for(int k=1;k<subsets.size()-1;k++){
                 denominatorLength = subsets.at(k).size();
                 auto im = freqItemSetsAndNumber.find(int(denominatorLength));
                 auto in = im->second.find(subsets.at(k));
-                denominator = in->second;
+                denominator = in->second; //#X
+                // Confidence(A->B) = Prob(B/A) = Support(A->B)/Support(A) = #(X&Y)/#X
                 if(float(numerator)/float(denominator)>=confidence||denominator==0){
                     if(show){
                         if(file_.is_open()){
                             for(int m=0;m<subsets.at(k).size()-1;m++){
-                                cout << subsets.at(k).at(m) << ",";
+                                //cout << subsets.at(k).at(m) << ",";
                                 file_ << subsets.at(k).at(m) << ",";
                             }
-                            cout << subsets.at(k).at(subsets.at(k).size()-1) << " -> " ;
+                            //cout << subsets.at(k).at(subsets.at(k).size()-1) << " -> " ;
                             file_ << subsets.at(k).at(subsets.at(k).size()-1) << " -> " ;
                             remain = getRemain(result.at(i).at(j), subsets.at(k));
                             for(int n=0;n<remain.size()-1;n++){
-                                cout << remain.at(n) << ",";
+                                //cout << remain.at(n) << ",";
                                 file_ << remain.at(n) << ",";
                             }
-                            cout << remain.at(remain.size()-1) << " ";
+                            //cout << remain.at(remain.size()-1) << " ";
                             file_ << remain.at(remain.size()-1) << " ";
-                            cout << "(" << std::fixed << std::setprecision(2) << float(numerator)/float(lineNumber)<< "," ;
+                            //cout << "(" << std::fixed << std::setprecision(2) << float(numerator)/float(lineNumber)<< "," ;
                             file_ << "(" << std::fixed << std::setprecision(2) << float(numerator)/float(lineNumber)<< "," ;
-                            cout << std::fixed << std::setprecision(2) << float(numerator)/float(denominator) << ")" << endl;
+                            //cout << std::fixed << std::setprecision(2) << float(numerator)/float(denominator) << ")" << endl;
                             file_ << std::fixed << std::setprecision(2) << float(numerator)/float(denominator) << ")" << endl;
                         }
                         
